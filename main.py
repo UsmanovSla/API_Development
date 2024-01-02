3b42bede0e1e181f393fe759dc827b3cff5cd808
UsmanovSla <usmanov@seznam.cz>
2024-01-02 14:23:10 +0100

    Functions: root/GET, creat_posts/POST

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('Hello World')
+from fastapi import Body, FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello world!!!"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+
+@app.post("/createposts")
+def create_posts(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"new_post": f"title: {payLoad['title']}, content: {payLoad['content']}"}
