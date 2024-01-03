f51a60377a1797c3af7687449cb869a382ce9c25
UsmanovSla <usmanov@seznam.cz>
2024-01-03 13:23:08 +0100

    Next steps: Function Delete post

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+# uvicorn main:app --reload 
+
 import random
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Response, status
 from pydantic import BaseModel
 
 
@@ -19,6 +21,18 @@ my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1
             ]
 
 
+def find_post(id):
+    for post in my_posts:
+        if post["id"] == id:
+            return post
+
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
+
+
 @app.get("/")
 def root():
     return {"message": "Hello world!!!"}
@@ -29,7 +43,7 @@ def get_posts():
     return {"data": my_posts}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.model_dump()
     post_dict['id'] = random.randint(0, 10_000_000)
@@ -38,6 +52,23 @@ def create_posts(post: Post):
 
 
 @app.get('/posts/{id}')
-def get_post(id):
-    print(id)
-    return {"post_detaile": f"Post with id: {id}"}
+def get_post(id: int):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with {id=} was not found")
+    return {"post_detail": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    # deleting post
+    # find the index i nthe array
+    index = find_index_post(id)
+
+    if not index:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with {id=} was not exist")
+
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
